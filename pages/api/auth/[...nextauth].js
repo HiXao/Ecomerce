ba8cc36bcaa9dc47ac615e6a9a1fe7b4b8e25844
unknown <haonak8at3@gmail.com>
2023-11-16 11:08:07 +0700

    chang admin email

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import GoogleProvider from 'next-auth/providers/google'
 import { MongoDBAdapter } from '@auth/mongodb-adapter';
 import clientPromise from '@/lib/mongodb';
 
-const adminEmails = ['vubakhoi3792@gmail.com'];
+const adminEmails = ['haonak8at3@gmail.com'];
 
 export const authOptions = {
   providers: [
